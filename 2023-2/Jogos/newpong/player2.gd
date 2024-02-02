d4ec714bba66d586894f781e94b857af31ab5940
Anna G <85114312+Annagmo@users.noreply.github.com>
2024-02-02 16:33:17 -0300

    Add files via upload

---
+++ 2023-2/Jogos/newpong/player2.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+
+export var a = true
+var move = Vector2()
+var direction
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	if a:
+		if Input.is_key_pressed(KEY_LEFT):
+			move.y = -300
+		elif Input.is_key_pressed(KEY_RIGHT):
+			move.y = 300
+		else:
+			move.y = 0
+	else:
+		if Input.is_key_pressed(KEY_Z):
+			move.y = -300
+		elif Input.is_key_pressed(KEY_X):
+			move.y = 300
+		else:
+			move.y = 0
+# warning-ignore:return_value_discarded
+	move_and_slide(move)
+
+
+# warning-ignore:unused_argument
+func _on_top_child_entered_tree(node):
+	pass # Replace with function body.
+
+
+# warning-ignore:unused_argument
+func _on_bott_child_entered_tree(node):
+	pass # Replace with function body.
+
+
+# warning-ignore:unused_argument
+func _on_left_child_entered_tree(node):
+	pass # Replace with function body.
+
+
+# warning-ignore:unused_argument
+func _on_right_child_entered_tree(node):
+	pass # Replace with function body.
